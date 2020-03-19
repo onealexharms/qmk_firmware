b8558d76a759929dcc406f6bbd89a00247799628
Alex Harms <onealexharms@gmail.com>
2020-03-18 22:09:25 -0400

    Changed some things

--- keyboards/ergodox_ez/keymaps/alex/rules.mk
+++ keyboards/ergodox_ez/keymaps/alex/rules.mk
@@ -2,5 +2,4 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LINK_TIME_OPTIMIZATION_ENABLE = yes
 COMMAND_ENABLE = no
-AUTO_SHIFT_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
