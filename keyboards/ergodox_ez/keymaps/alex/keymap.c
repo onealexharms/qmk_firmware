05c62eb07e18b74152a2fff9e5d55075dded0e9e
Alex Harms <onealexharms@gmail.com>
2020-03-30 16:01:58 -0400

    Make thumbpad more like Kyria will be

--- keyboards/ergodox_ez/keymaps/alex/keymap.c
+++ keyboards/ergodox_ez/keymaps/alex/keymap.c
@@ -112,20 +112,20 @@
       L_CTRL     ,    A_PARENS   ,     S_MOUSE   ,     D_D_PAD   ,   F_SYMBOLS   ,  G_NUMBERS    ,                                                                                                                     KC_H     ,     J_ESCAPE   ,       KC_K    ,       KC_L    , RCTL_SEMICOLON,    KC_QUOTE   , \
       L_SHIFT    ,    Z_HYPER    ,        KC_X   ,     KC_C      ,       KC_V    ,       KC_B    ,    _______    ,                                                                                   _______     ,     KC_N     ,        KC_M    ,    KC_COMMA   ,      PERIOD   ,    KC_SLASH   ,   R_SHIFT     , \
       L_ALT      ,    _______    ,     _______   ,     _______   ,     _______   ,                                                                                                                                                     _______   ,     _______   ,     _______   ,    _______    ,   BROKEN      , \
-                                                                                                                     SHIFT_TAB   ,    KC_CAPS    ,                    _______    ,   _______     ,               \
-                                                                                                                                      _______    ,                    _______    ,                               \
-                                                                                                       ENTER     ,  TAB_OR_CMD   ,   KC_ESCAPE   ,                    TO(BASE)   ,  BKSP_OR_CMD  ,    SPACE      \
+                                                                                                                       KC_CAPS   ,     TO(BASE)  ,                    TO(BASE)   ,      KC_0     ,               \
+                                                                                                                                       _______   ,                    _______    ,                               \
+                                                                                                       ENTER     ,  TAB_OR_CMD   ,     _______   ,                    _______    ,  BKSP_OR_CMD  ,    SPACE      \
 )
 
 #define NUMBER_KEYS LAYOUT_ergodox_pretty(\
-       _______   ,     _______   ,     _______   ,     _______   ,    _______    ,    _______    ,    LOCKED     ,                                                                                     LOCKED    ,     LOCKED    ,     LOCKED    ,     LOCKED    ,     LOCKED    ,    LOCKED     ,    DIVIDE     ,\
+       _______   ,     _______   ,     _______   ,     _______   ,    _______    ,    _______    ,    LOCKED     ,                                                                                    LOCKED     ,     LOCKED    ,     LOCKED    ,     LOCKED    ,     LOCKED    ,    LOCKED     ,    DIVIDE     ,\
        _______   ,     _______   ,     _______   ,     _______   ,    _______    ,    _______    ,    _______    ,                                                                                    _______    ,     _______   ,        KC_7   ,      KC_8     ,     KC_9      ,    _______    ,    MULTIPLY   ,\
        _______   ,     _______   ,     _______   ,     _______   ,    _______    ,    _______    ,                                                                                                                     _______   ,        KC_4   ,      KC_5     ,     KC_6      ,    _______    ,    MINUS      ,\
        _______   ,     _______   ,     _______   ,     _______   ,    _______    ,    _______    ,    _______    ,                                                                                    EQUAL      ,      KC_0     ,        KC_1   ,      KC_2     ,     KC_3      ,    _______    ,    PLUS       ,\
        _______   ,     _______   ,     _______   ,     _______   ,    _______    ,                                                                                                                                                     DECIMAL   ,     _______   ,     KC_0      ,    _______    ,    BROKEN     ,\
                                                                                                                        _______   ,     _______   ,                     _______   ,     _______   ,               \
                                                                                                                                        _______   ,                     _______   ,                               \
-                                                                                                       ENTER     ,  TAB_OR_CMD   ,   KC_ESCAPE   ,                    TO(BASE)   ,  BKSP_OR_CMD  ,    SPACE      \
+                                                                                                       ENTER     ,  TAB_OR_CMD   ,     _______   ,                     _______   ,  BKSP_OR_CMD  ,     KC_0      \
 )
 
 #define PAREN_KEYS LAYOUT_ergodox_pretty(\
