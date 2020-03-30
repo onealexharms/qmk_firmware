13cb8f126e5bd12d5d820dfeee00c6c1c221c324
Alex Harms <onealexharms@gmail.com>
2020-03-30 16:09:28 -0400

    Remove bottom layer keys that don't exist on the Kryia

--- keyboards/ergodox_ez/keymaps/alex/keymap.c
+++ keyboards/ergodox_ez/keymaps/alex/keymap.c
@@ -33,6 +33,7 @@
 #define LOCKED KC_TRANSPARENT // KEEP TRANPARENT
 #define BLANK KC_NO // DON'T ALLOW TRANSPARENCY
 #define BROKEN KC_NO 
+#define NOTHING KC_NO
 
 // some stuff to prevent mod tap keys from
 // doing other than what i am trying to do
@@ -107,11 +108,11 @@
 #define PURPLE 75,0,200
 
 #define ALPHA_KEYS LAYOUT_ergodox_pretty(\
-      TO(BASE)   ,   TO(COLORS)  ,     _______   ,     _______   ,     _______   ,     _______   ,    _______    ,                                                                                    TO(BASE)   ,  TO(NUMBERS)  ,   TO(PARENS)  ,  TO(SYMBOLS)  ,   TO(MOUSE)   ,   MO(D_PAD)   ,     BLANK     , \
-      _______    ,      KC_Q     ,        KC_W   ,        KC_E   ,        KC_R   ,       KC_T    ,    _______    ,                                                                                    _______    ,     KC_Y     ,        KC_U    ,       KC_I    ,       KC_O    ,       KC_P    ,    KC_0       , \
+      TO(BASE)   ,   TO(COLORS)  ,     _______   ,     _______   ,     _______   ,     _______   ,    _______    ,                                                                                   TO(BASE)    ,  TO(NUMBERS)  ,   TO(PARENS)  ,  TO(SYMBOLS)  ,   TO(MOUSE)   ,   MO(D_PAD)   ,     BLANK     , \
+      _______    ,      KC_Q     ,        KC_W   ,        KC_E   ,        KC_R   ,       KC_T    ,    _______    ,                                                                                   _______     ,     KC_Y     ,        KC_U    ,       KC_I    ,       KC_O    ,       KC_P    ,    KC_0       , \
       L_CTRL     ,    A_PARENS   ,     S_MOUSE   ,     D_D_PAD   ,   F_SYMBOLS   ,  G_NUMBERS    ,                                                                                                                     KC_H     ,     J_ESCAPE   ,       KC_K    ,       KC_L    , RCTL_SEMICOLON,    KC_QUOTE   , \
       L_SHIFT    ,    Z_HYPER    ,        KC_X   ,     KC_C      ,       KC_V    ,       KC_B    ,    _______    ,                                                                                   _______     ,     KC_N     ,        KC_M    ,    KC_COMMA   ,      PERIOD   ,    KC_SLASH   ,   R_SHIFT     , \
-      L_ALT      ,    _______    ,     _______   ,     _______   ,     _______   ,                                                                                                                                                     _______   ,     _______   ,     _______   ,    _______    ,   BROKEN      , \
+      NOTHING    ,    NOTHING    ,       L_ALT   ,     _______   ,     _______   ,                                                                                                                                                     _______   ,     _______   ,     _______   ,    NOTHING    ,   BROKEN      , \
                                                                                                                        KC_CAPS   ,     TO(BASE)  ,                    TO(BASE)   ,      KC_0     ,               \
                                                                                                                                        _______   ,                    _______    ,                               \
                                                                                                        ENTER     ,  TAB_OR_CMD   ,     _______   ,                    _______    ,  BKSP_OR_CMD  ,    SPACE      \
