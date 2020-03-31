fbaf8c79b3921c4fddb3e1e2ec759752b5444d9d
Alex Harms <onealexharms@gmail.com>
2020-03-31 17:27:35 -0400

    Remove unneeded out-of-the-box defines

--- keyboards/ergodox_ez/keymaps/alex/keymap.c
+++ keyboards/ergodox_ez/keymaps/alex/keymap.c
@@ -12,23 +12,6 @@
 #include "keymap_jp.h"
 #include "keymap_bepo.h"
 
-#define KC_MAC_UNDO LGUI(KC_Z)
-#define KC_MAC_CUT LGUI(KC_X)
-#define KC_MAC_COPY LGUI(KC_C)
-#define KC_MAC_PASTE LGUI(KC_V)
-#define KC_PC_UNDO LCTL(KC_Z)
-#define KC_PC_CUT LCTL(KC_X)
-#define KC_PC_COPY LCTL(KC_C)
-#define KC_PC_PASTE LCTL(KC_V)
-#define ES_LESS_MAC KC_GRAVE
-#define ES_GRTR_MAC LSFT(KC_GRAVE)
-#define ES_BSLS_MAC ALGR(KC_6)
-#define NO_PIPE_ALT KC_GRAVE
-#define NO_BSLS_ALT KC_EQUAL
-
-// above is out of the box
-// below is me
-
 // utility things 
 #define XXXXXXXX KC_NO
 
