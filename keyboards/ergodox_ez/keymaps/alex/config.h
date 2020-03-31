4da76cdf4d305877b298c7ca2e132ac184cb20b5
Alex Harms <onealexharms@gmail.com>
2020-03-31 14:02:16 -0400

    Trying some settings to make mod typing better

--- keyboards/ergodox_ez/keymaps/alex/config.h
+++ keyboards/ergodox_ez/keymaps/alex/config.h
@@ -2,3 +2,6 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
