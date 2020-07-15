81dc1af5a19e3bf9d1d46169a490071d9a8b439e
Alex Harms <onealexharms@gmail.com>
2020-07-14 22:29:58 -0400

    Show parens layer on display

--- keyboards/kyria/keymaps/onealexharms/keymap.c
+++ keyboards/kyria/keymaps/onealexharms/keymap.c
@@ -23,10 +23,10 @@
 #define SPACE KC_SPACE
 #define R_CMD_ESC MT(MOD_RGUI, KC_ESC)
 #define RIGHT_ALT KC_RALT
-#define L_SHIFT KC_LCTL
-#define L_CTRL KC_LSFT
+#define L_SHIFT KC_LSFT
+#define L_CTRL KC_LCTL
 #define CAPSLOCK KC_CAPSLOCK
-#define R_SHIFT KC_RCTL
+#define R_SHIFT KC_RSFT
 #define CTRL_QUOT MT(MOD_RCTL, KC_QUOT)
 #define NUMLOCK TG(NUMBERS)
 
@@ -130,6 +130,7 @@ enum layers {
                                  ALPHA   ,  LEFT_ALT , L_CMD_ESC,       ENTER  , TAB  ,       BACKSPACE,   SPACE,  R_CMD_ESC,  RIGHT_ALT   ,  ALPHA                                 \
   ) 
 
+
 const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
   [ALPHA] = ALPHA_KEYS,
   [NUMBERS] = NUMBER_KEYS, 
@@ -163,28 +164,16 @@ static void render_kyria_logo(void) {
     oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
 }
 
-static void render_qmk_logo(void) {
-  static const char PROGMEM qmk_logo[] = {
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-
-  oled_write_P(qmk_logo, false);
-}
-
-static void render_status(void) {
-    // QMK Logo and version information
-    render_qmk_logo();
-    oled_write_P(PSTR("       Kyria rev1.0\n\n"), false);
-
-    // Host Keyboard Layer Status
-    // oled_write_P(PSTR("Layer: "), false);
+static void display_layer_name(void) {
     switch (get_highest_layer(layer_state)) {
         case ALPHA:
             oled_write_P(PSTR("ALPHA\n"), false);
             break;
         case NUMBERS:
             oled_write_P(PSTR("NUMBERS\n"), false);
+            break;
+        case PARENS:
+            oled_write_P(PSTR("PARENS\n"), false);
             break;
         case SYMBOLS:
             oled_write_P(PSTR("SYMBOLS\n"), false);
@@ -203,7 +192,7 @@ static void render_status(void) {
 
 void oled_task_user(void) {
     if (is_keyboard_master()) {
-        render_status(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+        display_layer_name(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
     } else {
         render_kyria_logo();
     }
